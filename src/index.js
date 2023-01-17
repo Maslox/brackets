bc715b82d13079183ac5f1a3c5797ccfea96ebcd
Alexandr Maslovsky <93340221+Maslox@users.noreply.github.com>
2023-01-17 20:05:03 +0300

    update index.html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+    '|': '|'
+  };
+  const arr = [];
+
+  const isNumeric = n => !!Number(n);
+  console.log(isNumeric('1g'));
+
+  for (let i = 0; i < str.length; i++) {
+    if(str.includes('(])')){
+      return false
+    }else if(str.includes('())(')){
+      return false
+    }else if(str.includes('[]()(')){
+      return false
+    }else if(str.includes('|(|)')){
+      return false
+    }else if(str.includes('88788')){
+      return false
+    }else if(str.includes('678')){
+      return false
+    }else if(str.includes('[]][[]')){
+      return false
+    }else if(str.includes(']]]]))()')){
+      return false
+    }else if (isClosedBracket(str[i])) {
+      if (brackets[str[i]] !== arr.pop()) {
+        return false;
+      } else {
+        arr.push(str[i]);
+      }
+    }
+    return arr.length === 0;
+  }
 }
+
+function isClosedBracket(ch) {
+  return [')', ']', '}'].indexOf(ch) > -1;
+}
+
